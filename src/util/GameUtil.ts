4ef353085d1513e3f2b7e4b71eb2a1085d69152e
xiaomo <xiaomo@xiaomo.info>
2017-07-21 13:53:55 +0800

    target

--- src/util/GameUtil.ts
+++ src/util/GameUtil.ts
@@ -1,8 +1,28 @@
 // TypeScript file
 
 
-class GameUtil{
-        
+class GameUtil extends egret.EventDispatcher {
+    images: { [url: string]: egret.BitmapData } = {};
+    private resCount = 0;
+
+    /**
+     * 加载图片
+     * @param {string[]} urls
+     */
+    loadImages(urls: string[]) {
+        this.resCount = urls.length;
+        urls.forEach(url => {
+            const loader = new egret.ImageLoader();
+            const finishHandler = e => {
+                this.images[url] = loader.data;
+                if (this.resCount == Object.keys(this.images).length)
+                    this.dispatchEventWith(egret.Event.COMPLETE)
+            };
+            loader.once(egret.Event.COMPLETE, finishHandler, this);
+            loader.once(egret.IOErrorEvent.IO_ERROR, finishHandler, this);
+            loader.load(url);
+        });
+    }
 
 
 }
