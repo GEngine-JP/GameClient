2d030a793719031f8e0129068f9660d7aece17c1
xiaomo <xiaomo@xiaomo.info>
2017-07-27 11:57:07 +0800

    publish

---
+++ src/uiResource/Bag/BagBinder.ts
@@ -0,0 +1,10 @@
+/** This is an automatically generated class by FairyGUI. Please do not modify it. **/
+
+module info.xiaomo.client.Bag {
+
+	export class BagBinder{
+		public static bindAll():void {
+			fairygui.UIObjectFactory.setPackageItemExtension(UIMain.URL, UIMain);
+		}
+	}
+}
---
+++ src/uiResource/Bag/UIMain.ts
@@ -0,0 +1,25 @@
+/** This is an automatically generated class by FairyGUI. Please do not modify it. **/
+
+module info.xiaomo.client.Bag {
+
+	export class UIMain extends fairygui.GComponent {
+
+		public list:fairygui.GList;
+
+		public static URL:string = "ui://r5x6wd5vjh150";
+
+		public static createInstance():UIMain {
+			return <UIMain><any>(fairygui.UIPackage.createObject("Bag","Main"));
+		}
+
+		public constructor() {
+			super();
+		}
+
+		protected constructFromXML(xml: any): void {
+			super.constructFromXML(xml);
+
+			this.list = <fairygui.GList><any>(this.getChildAt(1));
+		}
+	}
+}
