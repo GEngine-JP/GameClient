5c78c47404dca006859a3b8807166249bccb75d6
xiaomo <xiaomo@xiaomo.info>
2017-07-27 16:58:51 +0800

    baseUI

--- src/ui/UIManager.ts
+++ src/ui/UIManager.ts
@@ -29,9 +29,6 @@ class UIManager {
     public static addPackages() {
         let stage: egret.Stage = MtwGame.Instance.stage;
         fairygui.UIPackage.addPackage("Login");
-        // fairygui.UIPackage.addPackage("Common");
-        // fairygui.UIPackage.addPackage("CreateRole");
-        // fairygui.UIPackage.addPackage("MainUI");
         fairygui.UIConfig.defaultFont = "宋体";
         stage.addChild(fairygui.GRoot.inst.displayObject);
     }
