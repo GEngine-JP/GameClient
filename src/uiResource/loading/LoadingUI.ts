93bb0d480aaaafaa0b6b3a527eaff043a9b1e028
xiaomo <xiaomo@xiaomo.info>
2017-07-29 18:02:13 +0800

    loading 居中

--- src/uiResource/loading/LoadingUI.ts
+++ src/uiResource/loading/LoadingUI.ts
@@ -10,10 +10,13 @@ class LoadingUI extends egret.Sprite {
     private createView(): void {
         this.textField = new egret.TextField();
         this.addChild(this.textField);
-        this.textField.y = 300;
         this.textField.width = 480;
         this.textField.height = 100;
+        this.textField.size = 50;
         this.textField.textAlign = "center";
+        this.textField.x = MtwGame.getInstance.stage.stageWidth / 4;
+        this.textField.y = MtwGame.getInstance.stage.stageHeight / 2;
+
     }
 
     public setProgress(current: number, total: number): void {
