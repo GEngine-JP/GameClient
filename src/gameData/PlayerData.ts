8a30126af7332c907652e2ace1fd3e5f036bebc1
xiaomo <xiaomo@xiaomo.info>
2017-08-05 17:27:37 +0800

    客户端改为unity

--- src/gameData/PlayerData.ts
+++
@@ -1,9 +0,0 @@
-class PlayerData {
-    public static readonly getInstance: PlayerData = new PlayerData();
-
-    public constructor() {
-    }
-
-    public uid: Long;
-    public randomName: string;
-}
