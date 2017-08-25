6b7f55fcf3c9a3a86b1b21671447cb5fb8585a25
xiaomo <xiaomo@xiaomo.info>
2017-08-25 18:09:20 +0800

    添加引用

--- AutofaceXlua/unity/AutoFaceAndXlua/Assets/Project/Code/Modules/SerializeUtil/SerializeUtil.cs
+++ AutofaceXlua/unity/AutoFaceAndXlua/Assets/Project/Code/Modules/SerializeUtil/SerializeUtil.cs
@@ -12,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using LHDepthCopy;
 using UnityEngine;
 
 namespace LHSerializeUtil
